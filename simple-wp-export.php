63bff5e97076837c5119c8e438905e70b1671f37
pete@petenelson.com <petenelson@fortress.local>
2016-06-10 19:08:52 -0500

    initial commit

---
+++ simple-wp-export.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Simple WP Export
+ * Version: 0.1-alpha
+ * Description: Export a list of posts and pages to a CSV file
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: simple-wp-export
+ * Domain Path: /languages
+ * @package Simple WP Export
+ */
